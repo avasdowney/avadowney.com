f6890e5515cfb19f9b8e0f25463391e3b8466845
avasdowney <avasdowney@gmail.com>
2021-08-14 21:11:20 -0400

    re-organized file structure

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
-import Home from './components/resumeContent/Home'
-import About from './components/resumeContent/About'
-import Education from './components/resumeContent/Education'
-import Work from './components/resumeContent/WorkExperience'
-import Other from './components/resumeContent/OtherExperience'
-import Projects from './components/resumeContent/Projects'
-import Contact from './components/resumeContent/Contact'
+import Navbar from './components/navbar/Navbar'
+import ResumeContent from './components/resumeContent/ResumeContent'
 
 import "./css/styles.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Home />
-    <About />
-    <Education />
-    <Work />
-    <Other />
-    <Projects />
-    <Contact />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
