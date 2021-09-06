96b511ab5979408046667be154b4163b93adac04
avasdowney <avasdowney@gmail.com>
2021-09-06 13:19:05 -0400

    changed background

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Navbar from './components/navbar/Navbar'
+import Home from './components/resumeContent/Home'
 import ResumeContent from './components/resumeContent/ResumeContent'
 
 import "./css/styles.css"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Navbar />
-  </React.StrictMode>,
+    <Home />
+  </BrowserRouter>,
   document.getElementById('root')
 );
