1e32fb0c58cd4572b78b67b0c531edc406500b06
avasdowney <avasdowney@gmail.com>
2021-12-19 20:40:36 -0500

    fixing 404 error

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import About from './components/resumeContent/About';
+import Education from './components/resumeContent/Education';
+import WorkExperience from './components/resumeContent/WorkExperience';
+import OtherExperience from './components/resumeContent/OtherExperience';
+import Projects from './components/resumeContent/Projects';
+import Contact from './components/resumeContent/Contact';
+  
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={About} />
+        <Route path='/about' component={About} />
+        <Route path='/education' component={Education} />
+        <Route path='/work-experience' component={WorkExperience} />
+        <Route path='/other-experience' component={OtherExperience} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
+  
+export default App;
