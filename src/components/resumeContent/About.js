e0a1fd5606230a36c54d13192be747760d2c92e2
avasdowney <avasdowney@gmail.com>
2022-01-02 15:08:58 -0500

    fixed comma in about section

--- src/components/resumeContent/About.js
+++ src/components/resumeContent/About.js
@@ -20,7 +20,7 @@ const About = () => {
                     I am a Junior honors student majoring in Computer Science, and minoring in both Mathematics and 
                     Data Science at the University of Hartford. I have been coding since I was little, starting off 
                     in Scratch, then moving onto Python and Java. I now have experience in many more languages such as 
-                    Clojure, and JavaScript, as well as many tools and frameworks including Git, Azure DevOps, JupyterNotebook
+                    Clojure, and JavaScript, as well as many tools and frameworks including Git, Azure DevOps, JupyterNotebook,
                     JUnit, TravisCI and more! I am currently working on several project both for myself, and collaborative 
                     projects with my clubs including updating this website, and building Discord bots, just to name a few! 
                     I am always looking for new projects to throw myself into to learn something new, whether it be new coding 
