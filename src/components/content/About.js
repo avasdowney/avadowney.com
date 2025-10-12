40875d8bd94cffbbb68e7e26a79476e368ccd0eb
avasdowney <avasdowney@gmail.com>
2025-10-11 21:42:30 -0400

    update look and feel of about page

--- src/components/content/About.js
+++ src/components/content/About.js
@@ -1,155 +1,125 @@
-import React, { useEffect } from 'react';
-import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, MoveIn, MoveOut, Sticky } from "react-scroll-motion";
-// unused vars for animation: Move, StickyIn, ZoomIn
+import React from 'react';
 
 import "../../css/styles.css";
 
 import pic from '../../images/ava_bitmoji.png';
+import GitHub from '../../images/GitHub-Mark-Light-64px.png';
+import LinkedIn from '../../images/LI-In-Bug.png';
+import Email from '../../images/mail.png';
+import DownArrow from '../../images/circle-arrow-down.png';
 
-// CSS Styles
-const imgStyle = {
-    display: 'inline-flex',
-    width: '25%',
-    margin: '0 auto',
-    marginRight: '5vw',
-    float: 'left',
-    borderRadius: '8px',
-    // borderBottom: 'dotted #808080'  
-};
-
-const mobileTxtStyle = {
-    marginTop: '5vh',
-    marginBottom: '0',
+const aboutContainer = {
+    minHeight: '80vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     textAlign: 'center',
+    paddingTop: '2vh',
+};
+const nameStyle = {
+    fontSize: '4rem',
+    fontWeight: 700,
+    color: '#fff',
+    marginBottom: '1.5rem',
+    letterSpacing: '2px',
+};
+const subtitleStyle = {
+    fontSize: '1.5rem',
+    color: '#e0e0e0',
+    marginBottom: '2.5rem',
+    fontWeight: 400,
+};
+const iconRow = {
+    display: 'flex',
+    gap: '2rem',
+    justifyContent: 'center',
+    marginBottom: '2.5rem',
+};
+const iconStyle = {
+    height: '2.5rem',
 };
 
-const mobileImgStyle = {
-    width: '50%',
-    margin: '0 auto',
-    display: 'flex'
+const scrollBtnStyle = {
+    marginTop: '2.5rem',
+    background: '#202020b3',
+    border: 'none',
+    borderRadius: '50%',
+    width: '3.5rem',
+    height: '3.5rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    boxShadow: '0 2px 8px #00000026',
+    transition: 'background 0.2s',
 };
 
-// check mobile
-var mobile = require('is-mobile');
-
-// animations
-const FadeUp = batch(FadeIn(), MoveIn(), Sticky());
-const AnimateTitle = batch(Fade(0, 1), MoveOut());
-const AnimateImg = batch(Fade(), MoveOut(-100, 0));
-
-// typing animation with rotating text
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 11) || 1000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+const arrowStyle = {
+  color: '#fff',
 };
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 750;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
+const bioSectionStyle = {
+  minHeight: '100vh',
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  margin: 0,
+  borderRadius: 0,
+  boxShadow: 'none',
+  width: '100vw',
+  maxWidth: '100vw',
+  padding: 0,
+  position: 'relative',
+  top: 0,
+  left: 0,
+  zIndex: 2,
 };
 
-// stuff that gets rendered out
 const About = () => {
-    useEffect(()=>{
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-phrase');
-            var period = elements[i].getAttribute('data-wait');
-            if (toRotate) {
-                new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    }, [])
-
-    if (!mobile()) {
-        return (
-            <header>
-                <div class = "content">
-                    {<br></br>}{<br></br>}                     
-                    <ScrollContainer>
-                        <ScrollPage page={0}> 
-                            <Animator animation={AnimateImg}>
-                                <img src={pic} alt="headshot" style={imgStyle}/>
-                            </Animator>
-                            <Animator animation={AnimateTitle}>
-                                {<br></br>}
-                                <h1 class="typewrite rotateColor" data-wait="2000"
-                                    data-phrase='[ "Hi, I&#39;m Ava.", "Welcome to my website!", "Scroll to learn more about me!" ]'>
-                                    <span class="wrap"></span>
-                                </h1>
-                            </Animator>
-                        </ScrollPage>
-                        <ScrollPage page={1}>
-                            <Animator animation={FadeUp}>
-                                <h2 class="rotateColor"> About </h2>
-                                <p>
-                                    I am a recent graduate of the University of Hartford with a B.S. in Computer Science, and a minor
-                                    in Data Science. I now work as a Data Engineer at Travelers as a part of the Business Insights 
-                                    and Analytics Leadership Development Program, or BI&A LDP! I have experience with languages such 
-                                    as Python, Java, Clojure, and JavaScript, as well as many tools and frameworks including Git, 
-                                    AWS, Terraform, and more.
-                                </p>
-                            </Animator>
-                        </ScrollPage>
-                    </ScrollContainer>
-                </div>
-            </header>
-        )
-    // for mobile version of website
-    } else {
-        return (
-            <header>
-                <div class = "content">
-                    {<br></br>}
-                    <img src={pic} alt="headshot" class="fade-in" style={mobileImgStyle}/>
-                    <h1 class="rotateColor" style={mobileTxtStyle}> Hi, I&#39;m Ava. </h1>
-                    <h2 class="rotateColor"> About </h2> 
-                    <p>
-                        I am a recent graduate of the University of Hartford with a B.S. in Computer Science, and a minor
-                        in Data Science. I now work as a Data Engineer at Travelers! I have experience with languages such 
-                        as Python, Java, Clojure, and JavaScript, as well as many tools and frameworks including Git, 
-                        AWS, Terraform, and more.
-                    </p>
-                </div>
-            </header>
-        )
+  const bioRef = React.useRef(null);
+  const handleScroll = () => {
+    if (bioRef.current) {
+      bioRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-}
+  };
+  return (
+    <>
+      <div style={aboutContainer}>
+        <img src={pic} alt="Ava Bitmoji" style={{
+          height: '30vh',
+          borderRadius: '50%',
+          objectFit: 'cover',
+          marginBottom: '.5rem',
+          boxShadow: '0 2px 16px rgba(0,0,0,0.18)'
+        }} />
+        <h1 class="rotateColor" style={nameStyle}>Ava Downey</h1>
+        <div style={subtitleStyle}>Data Engineer&nbsp;|&nbsp;Generative AI</div>
+        <div style={iconRow}>
+          <a href="https://github.com/avasdowney" target="_blank" rel="noreferrer">
+            <img src={GitHub} alt="GitHub" style={iconStyle} />
+          </a>
+          <a href="https://www.linkedin.com/in/ava-downey-9b59931aa/" target="_blank" rel="noreferrer">
+            <img src={LinkedIn} alt="LinkedIn" style={iconStyle} />
+          </a>
+          <a href="mailto:avasdowney@gmail.com">
+            <img src={Email} alt="Email" style={iconStyle} />
+          </a>
+        </div>
+        <button style={scrollBtnStyle} onClick={handleScroll} aria-label="Scroll for more">
+          <img src={DownArrow} alt="▼" style={arrowStyle} />
+        </button>
+      </div>
+      <section ref={bioRef} style={bioSectionStyle}>
+  <div style={{maxWidth: '600px', margin: '0 auto', color: '#e0e0e0', fontSize: '1.2rem', lineHeight: 1.7, padding: '2rem'}}>
+          I am a recent graduate of the University of Hartford with a B.S. in Computer Science, and a minor in Data Science. I now work as a Data Engineer at Travelers! I have experience with languages such as Python, Java, Clojure, and JavaScript, as well as many tools and frameworks including Git, AWS, Terraform, and more.
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/components/navbar/NavbarElements.js
+++ src/components/navbar/NavbarElements.js
@@ -9,11 +9,12 @@ export const Nav = styled.nav`
     justify-content: space-between;
     padding: 0.2rem / 2);
     z-index: 12;
+    position: sticky;
+    top: 0;
 `;
 
 // active currently does nothing
 export const NavLink = styled(Link)`
-    font-size: 1.1vmax;
     color: #808080;
     display: flex;
     align-items: center;
@@ -35,9 +36,6 @@ export const NavMenu = styled.div`
     /* Third Nav */
     /* width: 100vw;
     white-space: nowrap; */
-    @media screen and (max-width: 768px) {
-        font-size: 20%;
-    }
 `;
 
 export const NavBtn = styled.nav`
@@ -48,13 +46,9 @@ export const NavBtn = styled.nav`
     /* Third Nav */
     /* justify-content: flex-end;
     width: 100vw; */
-    @media screen and (max-width: 768px) {
-        font-size: 20%;
-    }
 `;
 
 export const NavBtnLink = styled(Link)`
-    font-size: 1.1vmax;
     border-radius: 2px;
     background: #808080;
     padding: .5vw .25vw;
